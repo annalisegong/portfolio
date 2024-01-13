b5cde7a3c63a827f2b58f9c5ef65b156aa72096d
annalisegong <annalise.gong@cuw.edu>
2024-01-12 19:29:16 -0600

    Update mainBody.vue

--- src/components/mainBody.vue
+++ src/components/mainBody.vue
@@ -21,7 +21,7 @@
   <v-container id="Projects" class="section-size">
       <v-divider color="#f4f2f5" :length="getDividerLength" thickness="5" class="divider-wt line-gradient"></v-divider>
       <h2 class="text-gradient">Projects</h2>
-      <v-row justify="space-evenly" flex-wrap="false">
+      <v-row justify="space-evenly">
           <ProjectCard :projects="projects" />
       </v-row>
     </v-container>
@@ -288,6 +288,7 @@ export default {
   .section-size{
     font-family: Mukta, Space-Grotesk, sans-serif;
     width: 100%;
+    flex-wrap: false;
     justify-content: center;
     align-items: center;
   }
